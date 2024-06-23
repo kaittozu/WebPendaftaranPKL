66ab4e8c9e4790ddd488a6d32e17bb77e0570be7
kaitozu <kaitozugraphic123@gmail.com>
2024-06-23 17:14:43 +0700

    membuat halaman utama

---
+++ app/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <div class="nav">
+        <div class="logo">
+            <p>Logo</p>
+        </div>
+
+        <div class="right-links">
+            <a href="#">Edit Profil</a>
+            <a href="logout.php"><button class="btn">Log Out</button></a>
+
+        </div>
+    </div>
+    <main>
+        <div class="main-box top">
+            <div class="top">
+                <div class="box">
+                    <p>Halo <b>Litto</b>, Selamat datang!</p>
+                </div>
+                <div class="box">
+                    <p>Email mu <b>litto@gmail.com</b></p>
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="box">
+                    <p>Umurmu adalah <b>17 Tahun</b></p>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
