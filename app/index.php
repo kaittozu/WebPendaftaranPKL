92ba0048218979e8c21b864ce72fe08a2395655f
kaitozu <kaitozugraphic123@gmail.com>
2024-06-23 14:56:06 +0700

    menambahkan form

--- app/index.php
+++ app/index.php
@@ -3,9 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
     <title>Pendaftaran PKL</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="box form-box">
+            <header>Login</header>
+            <form action="" method="post">
+
+                <div class="field input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" required>
+                </div>
+
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="text" name="password" id="password" required>
+                </div>
+
+                <div class="field input">
+                    <input type="submit" name="submit" value="Login"required>
+                </div>
+
+                <div class="links">
+                    Tidak punya akun? <a href="">Register sekarang</a>
+                </div>
+                
+            </form>
+        </div>
+    </div>
 </body>
 </html>
